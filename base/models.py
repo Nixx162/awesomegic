0dea59910553340c74bc44c13413df1d88f1b28d
Nixx162 <nicholas.jimmyalden@gmail.com>
2024-03-22 16:05:36 +0800

    create Transaction class

--- base/models.py
+++ base/models.py
@@ -4,5 +4,19 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class User(AbstractUser):
-    name = models.CharField(max_length=200)
-    balance = models.FloatField(default = 0)
+  name = models.CharField(max_length = 200)
+  balance = models.FloatField(default = 0)
+
+class Transaction(models.Model):
+  class Meta:
+    ordering = ["datetime"]
+
+  TRANSACTION_TYPE = {
+    "dep": "Deposit",
+    "wit": "Withdraw"
+  }
+
+  transaction_type = models.TextField(choices=TRANSACTION_TYPE)
+  user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+  datetime = models.DateTimeField()
+  value = models.PositiveIntegerField(null=True)
